383c1e28c3ba0cdb6964b5f52c0aead88962e90d
romullosantos <romullosantos@gmail.com>
2020-03-30 15:34:13 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-8-html5-video-player-19hxrp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-8-html5-video-player-19hxrp)
---
+++ src/app/app.component.ts
@@ -0,0 +1,45 @@
+import { Component, ViewChild, ElementRef } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  name = "Angular";
+  @ViewChild("videoPlayer", { static: false }) videoplayer: ElementRef;
+  isPlay: boolean = false;
+  toggleVideo(event: any) {
+    this.videoplayer.nativeElement.play();
+  }
+  playPause() {
+    var myVideo: any = document.getElementById("my_video_1");
+    if (myVideo.paused) myVideo.play();
+    else myVideo.pause();
+  }
+
+  makeBig() {
+    var myVideo: any = document.getElementById("my_video_1");
+    myVideo.width = 560;
+  }
+
+  makeSmall() {
+    var myVideo: any = document.getElementById("my_video_1");
+    myVideo.width = 320;
+  }
+
+  makeNormal() {
+    var myVideo: any = document.getElementById("my_video_1");
+    myVideo.width = 420;
+  }
+
+  skip(value) {
+    let video: any = document.getElementById("my_video_1");
+    video.currentTime += value;
+  }
+
+  restart() {
+    let video: any = document.getElementById("my_video_1");
+    video.currentTime = 0;
+  }
+}
